4a2aff9c43894863f659a3dd72e19054b0553fbe
nitish768 <nitish.singh1028@gmail.com>
2021-12-28 03:36:35 +0000

    Testing my python

---
+++ phi.py
@@ -0,0 +1,42 @@
+print("hello")
+"Hello"
+x="hello"
+type(x) #class strg
+x=5
+print(x)
+type(x) #class  int
+x=True
+type(x) #class bool True False first ltr cptl
+y = "hello this is nitish"
+type(y)
+print(y)
+y #hello this is nitish
+y[0] #h
+y[4] #o
+y[-1] #l
+y[6] #t slicing operator
+y[6:10] #this cutting operator it will show str between num
+y[2:4] #ll
+y[1:4] #ell start from given no exclude 1 no in last
+y[1:6] #ello
+y[1:6:1] #ello jump 1
+y[1:6:2] #el jump 2
+y
+y[:] #starting from last same ans
+y[::1] #hello this is nitish
+y[::2] #hloti svml
+y="this is my data welcome you"
+y
+y="this is my data \n welcome you" #scape sequence
+print(y) #explit line by line
+y="this is my data \t welcome you" #single cote and double cote is same in python bt not in other language
+print(y) #explit with space like tab
+x = "this is nitish searching for"
+"nitish" in x #True
+"ravi" in x #False
+y=2, 6, 89, "hai", 8.7
+type(y) #toupe data type
+
+
+
+
