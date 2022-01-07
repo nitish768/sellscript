ee9c08b8474d44e73ff3f85e024e6a148dfd53a9
nitish768 <nitish.singh1028@gmail.com>
2022-01-07 18:20:52 +0000

    all about web server

---
+++ test2.sh
@@ -0,0 +1,36 @@
+ifconfig enpos3
+rpm -q httpd
+dnf install httpd
+rpm -q httpd
+dnf list httpd
+pwd
+ls
+cd /var/www/html/
+pwd
+ls
+cd /root/
+ls
+pwd
+cd /var/www/html/
+pwd
+ls
+gedit jack.html
+cat jack.html
+pwd
+ls
+cd
+systemctl start httpd
+#apache web server
+ifconfig enpos3
+systemctl stop firewalld
+systemctl disable firewalld
+systemctl status httpd
+systemctl start httpd
+systemctl status httpd
+systemctl enable firewalld
+systemctl status httpd
+curl http://192.168.0.162/lw.html
+curl http://192.168.0.162/lw.html
+curl http://192.168.0.162/lw.html > my.html
+cat my html
+
