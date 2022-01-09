95b875da5af9cb2912216235b8bd52becc9ec8f6
nitish768 <nitish.singh1028@gmail.com>
2022-01-09 16:54:16 +0000

    network

---
+++ test6.sh
@@ -0,0 +1,26 @@
+xhost
+ifconfig enp0s3
+getenforce
+setenforce 0
+ifconfig enp0s3
+ifconfig enp0s3
+getenforce 
+export DISPLAY=:1
+firefox
+ifconfig enp0s3
+xhost -1
+xhost -
+ifconfig enp0s3
+firefox
+xhost
+firefox
+xhost
+fitrefox
+xhost +
+xhost
+ifconfig enp0s3
+setenforce 1
+getenforce
+firefox
+ls
+
