22dc6bbc12eb6350b1b24e7e6e3e92cff0899f2b
nitish768 <nitish.singh1028@gmail.com>
2022-01-09 16:39:37 +0000

    remote server

---
+++ test5.sh
@@ -0,0 +1,29 @@
+rpm -q epel-release
+cd /etc/yum.repos.d/
+ls
+yum list xclip
+cd
+xclip -o
+date
+date | xclip -i
+xclip -o
+xclip -o
+date | xclip -i
+xclip -o
+#next
+ifconfig enp0s3
+rpm -q openssh-server
+vim /etc/ssh/sshd_config
+systemctl status sshd
+ifconfig enp0s3
+rpm -q openssh-clients
+which ssh
+rpm -qf /user/bin/ssh
+ssh server
+ssh 172.20.10.11
+ssh 172.20.10.11 date
+ssh 172.20.10.11 date; cal; mkdir z1111
+ls
+ssh 172.20.10.11 "date; cal; mkdir z2222"
+ls
+
