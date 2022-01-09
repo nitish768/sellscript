31e095821df98bf3795ce8c118e6f2551589ca0f
nitish768 <nitish.singh1028@gmail.com>
2022-01-09 15:49:56 +0000

    date cal rnd

---
+++ test3.sh
@@ -0,0 +1,43 @@
+nitish code is typing
+
+nitish code is typing
+nitish code is typing
+nitish code is typing
+nitish code is typing
+nitish code is typing
+nitish code is typing
+nitish code is typing
+nitish code is typing
+nitish code is typing
+nitish code is typing
+nitish code is typing
+nitish code is typing
+nitish code is typing
+nitish code is typing
+#yy
+#p
+#no p
+#u
+#dd
+#:set number
+#vi +7
+#/name
+#vim -x name "for personal file
+cat >a.text
+cd ..
+rm -r -f -v a.txt/
+rm -rvf a.txt/
+ls
+rm hi.txt
+ls
+cat > b.txt
+ls
+cat > c.txt
+ls
+cp b.txt /root/
+cd /etc/httpd/
+cd
+cp -rv etc/httpd/  /root/
+ls
+
+
