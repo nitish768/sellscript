29184e1a0eecf8eb4f6b4001ca32fd9cfdb803e7
nitish768 <nitish.singh1028@gmail.com>
2022-01-10 03:17:15 +0000

    working on TUI

---
+++ phy1.py
@@ -0,0 +1,42 @@
+import os
+
+os.system("tput setaf 1")
+print("t\t\thay welcome to TUI thats make life simple")
+os.system("tput setaf 7")
+
+print("\t\t\t--------------------------------------- )
+
+
+print("""press 1: to see date
+press2: to check cal
+press3: conf web server
+press4: to create user
+press5: create file
+""")
+
+print("Enter your choice : " , end="")
+ch=input()
+
+print(ch)
+
+if int(ch) == 1:
+        os.system("date")
+elif int(ch) == 2:
+        os.system("cal")
+elif int(ch) == 3:
+        os.system("yum install httpd")
+elif int(ch) == 4:
+        print("can u plz user name: " , end='')
+        create user = input()
+        os.system("useradd {}".format (create_user))
+elif int(ch) == 5:
+        os.system("date")
+elif int(ch) == 1:
+        os.system("date")
+elif int(ch) == 1:
+        os.system("date")
+elif int(ch) == 1:
+        os.system("date")
+
+
+
