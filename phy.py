818d21050320edd1b304e4d36614c0c1f0130373
nitish768 <nitish.singh1028@gmail.com>
2022-01-10 02:58:19 +0000

    working on TUI

---
+++ phy.py
@@ -0,0 +1,15 @@
+import os
+
+os.system("tput setaf 1")
+print("t\t\thay welcome to TUI thats make life simple")
+os.system("tput setaf 7")
+
+print("\t\t\t--------------------------------------- )
+
+
+print("""press 1: to see date
+press2: to check cal
+press3: conf web server
+press4: to create user
+press5: create file
+""")
