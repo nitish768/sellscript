ea56fec4f19c13fb4a2527d1ecfcf3f2f110c1c4
nitish768 <nitish.singh1028@gmail.com>
2022-01-16 12:41:09 +0000

    about password and keygen

---
+++ phy2.py
@@ -0,0 +1,18 @@
+# x = gatepass()
+# import gatepass
+# x = gatepass.gatepass()
+# x = gatepass.gatepass("enter your pass")
+
+
+#ssh keygen
+#vim nitish.py
+#python nitish.py
+#ssh ip id 2nd
+#ssh-keygen
+#ssh-copy-id root@ip id 2nd
+#ifcongig enp0s3
+#ssh ip id 2nd 
+#ssh ip id 2nd date
+#ssh ip id 2nd cal
+#python3 nitish.py
+
